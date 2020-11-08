b4024cefd8e128480e33805fd9146d8c18ffa0b1
vinodmalali <70831777+vinodmalali@users.noreply.github.com>
2020-11-08 22:53:53 +0530

    Add files via upload

---
+++ day 1 .py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+1) RAINBOW
+
+# 2)
+# 
+
+# In[5]:
+
+
+print("LETS UPGRADE")
+
+
+# In[4]:
+
+
+x = input("enter the cost price\n")
+y = input("enter the selling price\n")
+if x<y:
+    print("profit")
+elif x>y:
+    print("loss")
+else:
+    print("neither")
+
+
+# 4)
+# 
+
+# In[ ]:
+
+
+x = input("enter the value\t")
+y=80
+
+print("x * y",x*y)
+
+
+# In[ ]:
+
+
+
+
